2d7ad80c874651f5ba81b8cac0f93efc5ea4a6ef
Сайденов Рамзан <rama050102@gmail.com>
2023-11-18 13:58:28 +0600

    craete model for Genre, Author, Book, Review, FavoriteBook

---
+++ BookCatalog/books/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
+
+
+class FavoriteBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
